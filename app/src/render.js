f2f59cfdf84251c6ab2e6dea7061994195897e0e
MaxMph <hurleymax255@gmail.com>
2025-05-10 17:55:27 -0400

    APP!!!!!!!!!!!!!!!!!!!!!!!

---
+++ app/src/render.js
@@ -0,0 +1,14 @@
+
+const money_text = document.getElementById('balance');
+const freemoneybutton = document.getElementById('calc_money');
+var money = 0
+money_text.textContent = "$0";
+
+freemoneybutton.onclick = calc_money;
+document.onCl
+
+function calc_money() {
+    console.log("IIFE executed");
+    money += 12;
+    money_text.textContent = "$" + String(money);
+}
