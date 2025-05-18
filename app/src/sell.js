579ec5e5e4e81bd3c7c094f460e7a02b12d7cf26
MaxMph <hurleymax255@gmail.com>
2025-05-17 20:20:46 -0400

    finaly feel like I am making signifacant progress
    things be working

--- app/src/sell.js
+++ app/src/sell.js
@@ -1,26 +1,29 @@
 const shipment = document.getElementById('shipment_input');
 const outputtext = document.getElementById('end_val');
-const key = []
-document.getElementById('send').onclick = decode;
+const sell_button = document.getElementById('sell');
+const key = [-4, 8, 2, -3, 6, 1]
+document.getElementById('send').onclick = code_tested;
+sell_button.onclick = sold;
 
 let start_array = [];
 let shipment_price = 0;
 
-let valid_code = true;
+function code_tested() {
+	if (check_validity() == true){
+		_validcode();
+	} else {
+		_invalidcode();
+	}
+}
 
-function decode() {
-	valid_code = true;
-	//console.log(JSON.parse(shipment.value));
+function check_validity() {
+	let valid_code = true;
 
-	//start_array = JSON.parse(shipment.value);
-	//for (let i = 0; )
 	start_array = shipment.value.trim().split(" ").map(Number)
-	//start_array = shipment.value.split();
+
+	//check if code is all numbers
 	for (let i = 0; i < start_array.length; i++) {
 		let curnum = start_array[i]
-		//console.log(curnum);
-		//console.log("ababarfer");
-		//console.log(start_array.length);
 
 		if (isNaN(curnum)) {
 			valid_code = false;
@@ -29,40 +32,47 @@ function decode() {
 
 	}
 
-	//console.log(start_array[start_array.length - 2]);
+	//check if code length is validated
 	if (start_array[start_array.length - 2] !== (10 - (start_array.length - 2))) {
-		valid_code = false
-		console.log(start_array.length - 2)
+		valid_code = false;
+		console.log(start_array.length - 2);
 	}
 
-	if (valid_code == true){
-			_validcode();
-	} else {
-			_invalidcode();
+	return valid_code;
+}
+
+function decode(){
+	let final_value = 0;
+	let final_text = "";
+	start_array.pop();
+	start_array.pop();
+	start_array = start_array.reverse();
+
+	//final text setup
+	for (let i = 0; i < start_array.length; i++) {
+		start_array[i] = start_array[i] / key[i];
+		final_text = final_text + String(start_array[i]);
+		//final_value += start_array[i]
 	}
-
-	//start_array = shipment.value.split();
-	//for (let i = 0; i < start_array.length; i++)
-	//	console.log(start_array[i]);
-	//	console.log("ababarfer")
-	//console.log(start_array.length);
-
-	//outputtext.textContent = shipment.value;
+	return final_text //String(final_value) //final_text
+	//start_array.remove(start_array.lastIndex);
+	//start_array.remove(start_array.lastIndex);
 }
 
 function _validcode() {
-	//outputtext.textContent = shipment.value;
 	console.log("valid_code");
 	outputtext.textContent = "";
-	for (let i = 0; i < start_array.length; i++) {
-		outputtext.textContent = outputtext.textContent + String(start_array[i]);
-
-
-	}
+	outputtext.textContent = "$" + decode();
+	sell_button.disabled = false;
 }
 
 function _invalidcode() {
-	outputtext.textContent = "Invalid Code"
+	outputtext.textContent = "Invalid Code";
 }
 //[4, 72, -16, 7, 7]
 //4 72 -16 7 7
+
+function sold() {
+	outputtext.textContent = "";
+	sell_button.disabled = true;
+}
