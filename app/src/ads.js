90b4c174abbd4b28306a22e28ceac1c59982884f
MaxMph <hurleymax255@gmail.com>
2025-05-21 09:37:22 -0400

    laptop switch

--- app/src/ads.js
+++ app/src/ads.js
@@ -1,7 +1,15 @@
-const ads_L = getElementById('ads_L')
-const ads_R = getElementById('ads_R')
-let ad_sources = [""]
+//const { fsync } = require("original-fs");
 
-func adify() {
+//const body = getElementById('bod').onLoad = adify;
+const ads_L = getElementById('ads_L');
+const ads_R = getElementById('ads_R');
+const ad_folder = '../ads';
+let ad_sources = [];
+
+function adify() {
+    console.log("onload")
+    //const fileList = fs.readdirSync('/ads');
+
+    //console.log(fileList);
 	
 }
