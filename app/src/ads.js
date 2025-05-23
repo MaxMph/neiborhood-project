e19d6206ef8fc9f2ad07457459f5da8cfa0d568e
MaxMph <hurleymax255@gmail.com>
2025-05-23 08:38:57 -0400

    AD RANDOMIZATION

--- app/src/ads.js
+++ app/src/ads.js
@@ -1,24 +1,39 @@
-<<<<<<< Updated upstream
-//const { fsync } = require("original-fs");
 
 //const body = getElementById('bod').onLoad = adify;
-const ads_L = getElementById('ads_L');
-const ads_R = getElementById('ads_R');
-const ad_folder = '../ads';
-let ad_sources = [];
+// const ads_L = getElementById('ads_L');
+// const ads_R = getElementById('ads_R');
+// const ad_folder = '../ads';
+// let ad_sources = [];
 
-function adify() {
-    console.log("onload")
+// function adify() {
+//     console.log("onload")
     //const fileList = fs.readdirSync('/ads');
 
     //console.log(fileList);
 	
-=======
-const ads_L = getElementById('ads_L');
-const ads_R = getElementById('ads_R');
-let ad_sources = ["../ads/FORGEHACK.gif", "../ads/EXOFIGHTS.gif", "../ads/102 helmet.png"];
 
-func adify() {
-	document.createElement('img');
->>>>>>> Stashed changes
+const ads_L = document.getElementById('ads_L');
+const ads_R = document.getElementById('ads_R');
+let ad_sources = ["../ads/FORGEHACK.gif", "../ads/EXOFIGHTS.gif", "../ads/102 helmet.png", "../ads/DRIFTMETAL ZONE.gif"];
+let ad_num = 20
+
+const body = document.getElementById('bod')
+bod.onload = adify;
+
+function adify() {
+	console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+	
+	for (let i = 0; i < ad_num; i++) {
+		rand_ad(ads_L);
+		rand_ad(ads_R);
+	}
+}
+
+function rand_ad(ad_holder) {
+	console.log("rand");
+	let img = document.createElement('img');
+	//img.src = "../ads/FORGEHACK.gif";
+	img.src = ad_sources[Math.floor(Math.random() * ad_sources.length)]
+	img.className = 'ad';
+	ad_holder.appendChild(img);
 }
