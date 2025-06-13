5881fd1ebfd102fcad8c2c46da541243f2e0eea3
MaxMph <hurleymax255@gmail.com>
2025-06-13 17:22:27 -0400

    I have partialy working godot side ticket decoding :)

--- neiborhood-project/project/deployment_door.gd
+++ neiborhood-project/project/deployment_door.gd
@@ -1,10 +1,14 @@
 extends CSGBox3D
 
 @onready var text_edit = $CanvasLayer/Control/PanelContainer/VBoxContainer/TextEdit
+@onready var invalid_warning = $CanvasLayer/Control/PanelContainer/VBoxContainer/Label
+var valid = true
+
 func interacted():
 	$CanvasLayer.visible = true
 	Global.in_menu = true
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	invalid_warning.visible = true
 	#get_tree().change_scene_to_file("res://project/main.tscn")
 
 
@@ -12,10 +16,30 @@ func _on_close_pressed() -> void:
 	$CanvasLayer.visible = false
 	Global.in_menu = false
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	$CanvasLayer/Control/PanelContainer/VBoxContainer/TextEdit.text = ""
+	text_edit.text = ""
 
 
 func _on_enter_pressed() -> void:
+	invalid_warning.visible = false
 	var inputcode: String = text_edit.text
-	if inputcode.split()
+	var decode = inputcode.split(" ")
+	var id
+	decode = decode.slice(3)
+	Global.drop_location = int(decode[0])
+	id = int(decode[1])
+	print(decode)
+	if decode.size() != Global.drop_location + 2:
+		valid = false
+		print(decode.size())
+		print(Global.drop_location + 1)
+	
+	for i in Global.drop_ids:
+		if i == id:
+			valid = false
+	
+	if valid == true:
+		Global.drop_ids.append(id)
 		get_tree().change_scene_to_file("res://project/main.tscn")
+	else:
+		text_edit.text = ""
+		$CanvasLayer/Control/PanelContainer/VBoxContainer/Label.visible = true
--- neiborhood-project/project/scripts/Global.gd
+++ neiborhood-project/project/scripts/Global.gd
@@ -5,6 +5,7 @@ var inv_items: Array = []
 var player
 
 var drop_location = 0
+var drop_ids: Array = []
 
 var shipments_made = 0
 #@onready var input_manager = $"Player/input manager"
